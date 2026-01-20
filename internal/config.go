35409fd817d83a5b9a512ea425e079183d4975dc
pabalyberdin <pabalyberdin@avito.ru>
2026-01-20 21:27:07 +0300

    Добавил приоритеты для конфигурационного файла (по убыванию): флаги - енвы - json-файл - дефолты

--- internal/config.go
+++ internal/config.go
@@ -2,8 +2,12 @@ package internal
 
 import (
 	"cmp"
+	"encoding/json"
 	"flag"
 	"os"
+	"strconv"
+
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -15,6 +19,16 @@ const (
 )
 
 type Config struct {
+	Host         string `json:"host"`
+	Port         int    `json:"port"`
+	DNS          string `json:"dns"`
+	MigratePath  string `json:"migrate_path"`
+	Debug        bool   `json:"debug"`
+	TaskCapacity int    `json:"task_capacity"`
+}
+
+type Flags struct {
+	ConfigPath   string
 	Host         string
 	Port         int
 	DNS          string
@@ -23,18 +37,133 @@ type Config struct {
 	TaskCapacity int
 }
 
-func ReadConfig() Config {
-	var config Config
-	flag.StringVar(&config.Host, "host", defHost, "Server host")
-	flag.IntVar(&config.Port, "port", defPort, "Server port")
-	flag.StringVar(&config.DNS, "dns", defDNS, "DB CONNECTION STRING")
-	flag.StringVar(&config.MigratePath, "migrate-path", defMigrationsPath, "Path to migrations folder")
-	flag.BoolVar(&config.Debug, "debug", false, "Debug mode (уровень логирования)")
-	flag.IntVar(&config.TaskCapacity, "task-capacity", defTaskCapacity, "Task capacity for delete")
-	flag.Parse()
+// Дефолты не указывал, так как заданы отдельно.
+func parseFlags() Flags {
+	var flags Flags
 
-	config.DNS = cmp.Or(os.Getenv("DB_DNS"), defDNS)
-	config.MigratePath = cmp.Or(os.Getenv("MIGRATE_PATH"), defMigrationsPath)
+	flag.StringVar(&flags.ConfigPath, "c", "", "Path to config file")
+
+	flag.StringVar(&flags.Host, "host", "", "Server host")
+	flag.IntVar(&flags.Port, "port", 0, "Server port")
+	flag.StringVar(&flags.DNS, "dns", "", "DB CONNECTION STRING")
+	flag.StringVar(&flags.MigratePath, "migrate-path", "", "Path to migrations folder")
+	flag.BoolVar(&flags.Debug, "debug", false, "Debug mode")
+	flag.IntVar(&flags.TaskCapacity, "task-capacity", 0, "Task capacity")
+
+	flag.Parse()
+	return flags
+}
+
+func configFromFlags(flags *Flags) Config {
+	return Config{
+		Host:         flags.Host,
+		Port:         flags.Port,
+		DNS:          flags.DNS,
+		MigratePath:  flags.MigratePath,
+		Debug:        flags.Debug,
+		TaskCapacity: flags.TaskCapacity,
+	}
+}
+
+func configFromEnv() Config {
+	cfg := Config{}
+
+	cfg.Host = os.Getenv("HOST")
+	cfg.Port, _ = strconv.Atoi(os.Getenv("PORT"))
+	cfg.DNS = os.Getenv("DB_DNS")
+	cfg.MigratePath = os.Getenv("MIGRATE_PATH")
+	cfg.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+	cfg.TaskCapacity, _ = strconv.Atoi(os.Getenv("TASK_CAPACITY"))
+
+	return cfg
+}
+
+func configFromFile(path string) Config {
+	cfg := Config{}
+
+	if path == "" {
+		log.Info().Msg("Config file path is empty")
+		return cfg
+	}
+
+	data, err := os.ReadFile(path)
+
+	if err != nil {
+		log.Info().Err(err).Msg("Config file read failed")
+		return cfg
+	}
+
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		log.Info().Err(err).Msg("Config file unmarshal failed")
+		return cfg
+	}
+
+	return cfg
+}
+
+func defaultConfig() Config {
+	return Config{
+		Host:         defHost,
+		Port:         defPort,
+		DNS:          defDNS,
+		MigratePath:  defMigrationsPath,
+		TaskCapacity: defTaskCapacity,
+	}
+}
+
+// ReadConfig - чтение конфига приложения.
+// Может не подойти при расширении/изменении, так как про cmp.Or возвращает zero values только после проверки всех аргументов.
+func ReadConfig() Config {
+	config := Config{}
+
+	flags := parseFlags()
+	flagCfg := configFromFlags(&flags)
+	envCfg := configFromEnv()
+	fileCfg := configFromFile(flags.ConfigPath)
+	defCfg := defaultConfig()
+
+	config.Host = cmp.Or(
+		flagCfg.Host,
+		envCfg.Host,
+		fileCfg.Host,
+		defCfg.Host,
+	)
+
+	config.Port = cmp.Or(
+		flagCfg.Port,
+		envCfg.Port,
+		fileCfg.Port,
+		defCfg.Port,
+	)
+
+	config.DNS = cmp.Or(
+		flagCfg.DNS,
+		envCfg.DNS,
+		fileCfg.DNS,
+		defCfg.DNS,
+	)
+
+	config.MigratePath = cmp.Or(
+		flagCfg.MigratePath,
+		envCfg.MigratePath,
+		fileCfg.MigratePath,
+		defCfg.MigratePath,
+	)
+
+	config.Debug = cmp.Or(
+		flagCfg.Debug,
+		envCfg.Debug,
+		fileCfg.Debug,
+		defCfg.Debug,
+	)
+
+	config.TaskCapacity = cmp.Or(
+		flagCfg.TaskCapacity,
+		envCfg.TaskCapacity,
+		fileCfg.TaskCapacity,
+		defCfg.TaskCapacity,
+	)
 
 	return config
 }
